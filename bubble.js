7753b2995fdc410c460d03b7df465f8a890583ec
Stephan-e <stephan.erasmus@rsaweb.net>
2018-01-08 16:32:55 +0200

    layout

--- bubble.js
+++ bubble.js
@@ -1,13 +1,22 @@
- var width = 1, height = 1;
+ var width = 2, height = 2;
+ var border=1;
+var bordercolor='black';
 
 var svg = d3.select('#chart')
     .append('svg')
-    .attr('viewBox', "0 0 500 500")
+    .attr('viewBox', "0 0 800 500")
     //.attr('width', width)
     .append('g')
-    .attr("transform", 'translate(250,300)');
+    .attr("transform", 'translate(400,300)');
 
-
+var borderPath = svg.append("rect")
+                .attr("x", -400)
+                .attr("y", -300)
+                .attr("height", 500)
+                .attr("width", 800)
+                .style("stroke", bordercolor)
+                .style("fill", "none")
+                .style("stroke-width", border);
 
 
 var defs = svg.append("defs");
@@ -18,7 +27,7 @@ var radiusScale = d3.scaleSqrt().domain([0, 100]).range([30, 100]);
 var xForce = d3.forceX(width / 2).strength(0.05);
 var yForce = d3.forceY(height / 2).strength(0.05);
 var manyBodyForce = d3.forceManyBody().strength(function (d) {
-    const force = Math.floor(Math.random() * 50);
+    const force = Math.floor(Math.random() * 5);
     return -0.07 * Math.pow(radiusScale(force), 2);
 
 });
@@ -85,7 +94,7 @@ function ready(err, root) {
 
     svgNodes.append('circle')
         .attr('r', function () {
-            return radiusScale(Math.floor(Math.random()*50));
+            return radiusScale(Math.floor(Math.random()*15));
         })
         .style('fill', function (d) {
             return 'url(#' + d.data.name +')';
@@ -96,6 +105,25 @@ function ready(err, root) {
      * Function that gets called on each time tick.
      */
     function tick() {
+        var u = d3.select('svg')
+        .selectAll('circle')
+        .data(nodes)
+
+      u.enter()
+        .append('circle')
+        .attr('r', function(d) {
+          return d.radius
+        })
+        .merge(u)
+        .attr('cx', function(d) {
+          return d.x
+        })
+        .attr('cy', function(d) {
+          return d.y
+        })
+
+      u.exit().remove()
+
         svgNodes.attr("transform", function (d) {
             return "translate(" + d.x + "," + d.y + ")";
         });
@@ -142,7 +170,7 @@ function ready(err, root) {
             // The maximum value that any circle will have (not radius)
             // this translates to a radius of 80
             // TODO: Refactor this to be varants elsewhere
-            var v = 300;
+            var v = 200;
 
             // Save the old value for setting back to original size
             d.data['activated'] = true;
@@ -169,6 +197,34 @@ function ready(err, root) {
         simulation.alphaTarget(1).restart();
     }
 }
+
+    function collide(node) {
+      var r = node.radius + 2,
+          nx1 = node.x - r,
+          nx2 = node.x + r,
+          ny1 = node.y - r,
+          ny2 = node.y + r;
+      return function(quad, x1, y1, x2, y2) {
+        if (quad.point && (quad.point !== node)) {
+          var x = node.x - quad.point.x,
+              y = node.y - quad.point.y,
+              l = Math.sqrt(x * x + y * y),
+              r = node.radius + quad.point.radius;
+          if (l < r) {
+            l = (l - r) / l * .5;
+            node.x -= x *= l;
+            node.y -= y *= l;
+            quad.point.x += x;
+            quad.point.y += y;
+          }
+        }
+        return x1 > nx2
+            || x2 < nx1
+            || y1 > ny2
+            || y2 < ny1;
+      };
+  }
+
 
 function getColours() {
     return {
